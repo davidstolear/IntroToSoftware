2a2a2679b7cf947c502c6d2036cc4656502a488f
davidstolear <31225024+davidstolear@users.noreply.github.com>
2017-09-06 22:45:36 -0400

    assignment 2 complete

--- listingController.js
+++ listingController.js
@@ -1,14 +1,38 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
-    $scope.detailedInfo = undefined;
+    $scope.detailedInfo = {};
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      	$scope.listings.push({
+          code: $scope.newEntry.code,
+      		name: $scope.newEntry.name,
+    /*        longitude: $scope.newEntry.coordinates.longitude,
+      		  latitude: $scope.newEntry.coordinates.latitude,*/
+      		address: $scope.newEntry.address
+      	});
+      	$scope.newEntry.name="";
+      	$scope.newEntry.code="";
+        $scope.newEntry.coordinates.latitude="";
+        $scope.newEntry.coordinates.longitude="";
+      	$scope.newEntry.address="";
+    };
+
+    $scope.deleteListing = function(Building) {
+      	var removedListing = $scope.listings.indexOf(Building);
+      	$scope.listings.splice(removedListing, 1);
+    };
+    $scope.showDetails = function(entries) {
+      $scope.detailedInfo=entries;
+      // $scope.detailedEntry.code=$scope.entries.code;
+      // $scope.detailedEntry.name=$scope.entries.name;
+      // $scope.detailedEntry.coordinates.latitude=$scope.entries.coordinates.latitude;
+      // $scope.detailedEntry.coordinates.longitude=$scope.entries.coordinates.longitude;
+      // $scope.detailedEntry.address=$scope.entries.address;
+    };
   }
 ]);
